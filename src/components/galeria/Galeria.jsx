e24b0312d42c08ff6a05bc79cc632d0cc00f4cd1
sebastian <sebapepe21@gmail.com>
2023-11-29 14:54:57 -0300

    Galeria de imagenes

--- src/components/galeria/Galeria.jsx
+++ src/components/galeria/Galeria.jsx
@@ -6,12 +6,29 @@ export default function Galeria() {
       <div className="galeria">
         <h2> Galería de Imágenes</h2>
         <div className="imagen-container">
-        <img src="/public/assets/lec.jpg" alt="" />
-        <img src="/public/assets/lec.jpg" alt="" />
-        <img src="/public/assets/lec.jpg" alt="" />
-        <img src="/public/assets/lec.jpg" alt="" />
-        <img src="/public/assets/lec.jpg" alt="" />
-        <img src="/public/assets/lec.jpg" alt="" />
+        <img src="./img/1.jpeg" alt="" />
+        <img src="./img/2.jpg" alt="" />
+        <img src="./img/3.jpeg" alt="" />
+        <img src="./img/6.jpeg" alt="" />
+        <img src="./img/7.jpeg" alt="" />
+        <img src="./img/8.jpeg" alt="" />
+        <img src="./img/9.jpeg" alt="" />
+        <img src="./img/10.jpeg" alt="" />
+        <img src="./img/11.jpeg" alt="" />
+        <img src="./img/12.jpeg" alt="" />
+        <img src="./img/13.jpeg" alt="" />
+        <img src="./img/14.jpeg" alt="" />
+        <img src="./img/15.jpeg" alt="" />
+        <img src="./img/16.jpeg" alt="" />
+        <img src="./img/17.jpeg" alt="" />
+        <img src="./img/18.jpeg" alt="" />
+        <img src="./img/19.jpeg" alt="" />
+        <img src="./img/20.jpeg" alt="" />
+        <img src="./img/21.jpeg" alt="" />
+        <img src="./img/22.jpeg" alt="" />
+        
+        
+        
         </div>
       </div>
     </>
