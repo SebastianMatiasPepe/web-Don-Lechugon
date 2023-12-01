4348b6291f1eb12309511ff9f1b7ec938d06bd41
matias062169 <matiasezequiel062169@gmali.com>
2023-12-01 16:58:20 -0300

    mapa añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import Header from "./components/header/Header"
-import Info from "./components/info/Info"
-import Slider from "./components/slider/Slider"
-import Galeria from "./components/galeria/Galeria"
-import Footer from "./components/footer/Footer"
-
-
+import Header from "./components/header/Header";
+import Info from "./components/info/Info";
+import Slider from "./components/slider/Slider";
+import Galeria from "./components/galeria/Galeria";
+import Footer from "./components/footer/Footer";
+import Map from "./components/map/Map";
 import "./App.css";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
       <Header />
       <Info />
       <Slider />
       <Galeria />
+      <div className="App">
+        <Map />
+      </div>
+      <br />
       <Footer />
-     
     </>
-  )
+  );
 }
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const Map = () => {
+  const position = [-38.05255918995808, -57.56565259304688]; // Latitud y longitud inicial
+
+  return (
+    <section className="container-map">
+      <div style={{ overflow: "hidden", width: "90%",  margin: '0 auto' }}>
+        <MapContainer
+          center={position}
+          zoom={13}
+          style={{ height: "400px", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </section>
+  );
+};
+
+export default Map;
